11e55da1b97b477b03880ef10c9da66afd8a37be
Fábio <fabio.s0@hotmail.com>
2023-04-13 00:04:46 -0300

    Atualizando infraestrutura

--- api/src/main/java/org/edu/unidep/infrastructure/impl/ProdutoImpl.java
+++ api/src/main/java/org/edu/unidep/infrastructure/impl/ProdutoImpl.java
@@ -35,4 +35,45 @@ public class ProdutoImpl implements PanacheRepository<Produto>, ProdutoRepositor
 	public void deletar(Produto produto) {
 		delete(produto);
 	}
+
+	@Override
+	public String buscarVendasPorUnidadeDeMedida(String descricao, String unidadeDeMedida) {
+		
+		String jpql = """				
+				SELECT SUM(totalDeVendas) FROM Produto p WHERE p.descricao = :descricao AND p.unidadeMedida = :unidadeDeMedida
+				""";
+		
+		return getEntityManager()
+				.createQuery(jpql)
+				.setParameter("unidadeDeMedida", unidadeDeMedida)
+				.setParameter("descricao", descricao)
+				.getSingleResult()
+				.toString();
+	}
+
+	@Override
+	public String buscarVendasPorReais(String descricao) {
+		
+		String jpql = """
+				SELECT SUM(totalDeVendas*preco) FROM Produto p WHERE p.descricao = :descricao
+				""";
+		return getEntityManager()
+				.createQuery(jpql)
+				.setParameter("descricao", descricao)
+				.getSingleResult()
+				.toString();
+	}
+
+	@Override
+	public String buscarTotalEmEstoque(String descricao) {
+		String jpql = """
+				SELECT SUM(estoque) FROM Produto p WHERE p.descricao = :descricao
+				""";
+				
+		return getEntityManager()
+				.createQuery(jpql)
+				.setParameter("descricao", descricao)
+				.getSingleResult()
+				.toString();
+	}
 }
