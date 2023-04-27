b9e48eb1ec00f34c3c95ca6cc6c9590a8a19b07a
Fábio <fabio.s0@hotmail.com>
2023-04-27 11:38:07 -0300

    Adicionando Swagger ao ExceptionMessage

--- api/src/main/java/org/edu/unidep/api/exceptionhandler/ExceptionMessage.java
+++ api/src/main/java/org/edu/unidep/api/exceptionhandler/ExceptionMessage.java
@@ -3,19 +3,27 @@ package org.edu.unidep.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class ExceptionMessage {
 
+	@Schema(example = "2007-12-03T10:15:30")
 	private LocalDateTime timeStamp;
 	
+	@Schema(example = "EntidadeNaoEncontradaException")
 	private String type;
 	
+	@Schema(example = "Não existe cadastro de pessoa com o id: 10")
 	private String userMessage;
 	
 	private Set<FieldError> fieldErrors;
 	
 	public static class FieldError{
 		
+		@Schema(example = "Cpf")
 		private String nameField;
+		
+		@Schema(example = "O CPF informado deve ser válido de acordo com as normas da Receita Federal Brasileira.")
 		private String userMessage;
 		
 		public FieldError(String nameField, String userMessage) {
