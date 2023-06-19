9b99f6bad789e74e0d74317960d0253c8204288f
Fábio <fabio.s0@hotmail.com>
2023-06-18 22:59:39 -0300

    Update ProdutoRepository.java

--- Richardson_Level_3/api/src/main/java/org/edu/unidep/domain/repository/ProdutoRepository.java
+++ Richardson_Level_3/api/src/main/java/org/edu/unidep/domain/repository/ProdutoRepository.java
@@ -3,13 +3,11 @@ package org.edu.unidep.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.edu.unidep.domain.model.FotoProduto;
 import org.edu.unidep.domain.model.Produto;
 
 public interface ProdutoRepository {
 	List<Produto> listarProdutos();
 	Optional<Produto> buscarProdutoPeloCodigo(Long id);
-	Optional<FotoProduto> buscarFotoProdutoPeloCodigo(Long produtoId);
 	String buscarVendasPorUnidadeDeMedida(String descricao, String unidadeDeMedida);
 	String buscarVendasPorReais(String descricao);
 	String buscarTotalEmEstoque(String descricao);
